9ed820ada5a49142c0f337c035383e99c37dae45
Mina Karam <minakaram.me@gmail.com>
2024-03-04 00:58:48 +0200

    Update

--- hareettashfer/main.py
+++ hareettashfer/main.py
@@ -1,11 +1,9 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
-from hareettashfer_ui import Ui_HareetTashfer
+from HareetTashferApp import HareetTashferApp
 import sys
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    HareetTashfer = QtWidgets.QMainWindow()
-    ui = Ui_HareetTashfer()
-    ui.setupUi(HareetTashfer)
-    HareetTashfer.show()
+    main_app = HareetTashferApp()
+    main_app.show()
     sys.exit(app.exec_())
